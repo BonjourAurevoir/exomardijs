4fe85c0436271e1a5ab9eb46f931a2f6d42a8ccf
BonjourAurevoir <superfuture13nrv@gmail.com>
2015-12-02 16:51:42 +0100

    arrays finis normalement

---
+++ script.js
@@ -0,0 +1,22 @@
+var myarray= array(3);
+var input = ["2", "4", "6"];
+for (i=0; i <3; i++)
+    myarray[i]= array (3)
+
+for (var i = 0; i < a.length; i++) {
+  console.log(a[i]);
+
+}
+
+function doublearr(input)
+{
+    if(typeof input === 'number') {
+        return 2 * input;
+
+    } else {
+        return alert("C'est pas un number");
+    }
+}
+
+}
+
